122d668b85599f8fd5f04f872c2c38035e70e4c1
Pog-Frog <60003686+Pog-Frog@users.noreply.github.com>
2024-09-30 00:37:00 +0300

    working server

--- src/schema/typeDefs.ts
+++ src/schema/typeDefs.ts
@@ -12,7 +12,7 @@ const typeDefs = gql`
 
     type Query {
         users: [User!]!
-        friends
+        user(id: ID!): User
     }
 `;
 
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,37 @@
-import { ApolloServer } from "@apollo/server";
 import typeDefs from "./schema/typeDefs";
 import resolvers from "./schema/resolvers";
-import { startStandaloneServer } from '@apollo/server/standalone';
+import { ApolloServer } from '@apollo/server';
+import { expressMiddleware } from '@apollo/server/express4';
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
+import express from 'express';
+import http from 'http';
+import cors from 'cors';
 
+async function startApolloServer(typeDefs: any, resolvers: any) {
+  const app = express();
+  const httpServer = http.createServer(app);
 
-const server = new ApolloServer({
-    typeDefs, resolvers
-});
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+  }) as any
 
+  await server.start();
+
+  app.use(
+    '/graphql',
+    cors<cors.CorsRequest>({ origin: "*", credentials: true }),
+    express.json(),
+    expressMiddleware(server, {
+      context: async ({ req }) => ({ token: req.headers.token }),
+    }),
+  );
+
+  await new Promise<void>((resolve) =>
+    httpServer.listen({ port: 4000 }, resolve) //run the server on port 4000
+  );
+  console.log('Server ready at http://localhost:4000/graphql');
+}
+
+startApolloServer(typeDefs, resolvers).catch(console.error);
